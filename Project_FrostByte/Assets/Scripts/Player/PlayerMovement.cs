5aa96aff15c6ee3e9fe333ad5899d9090a0d5e80
dryAgedBanana <b.cao@sintlucasedu.nl>
2025-01-13 08:18:13 +0100

    replace the old code with the new code

--- Project_FrostByte/Assets/Scripts/Player/PlayerMovement.cs
+++ Project_FrostByte/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,104 +1,137 @@
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement Speed")]
-    public float speed = 5f;
-    public float sprintSpeed = 10f;
-    public float crouchSpeed = 2.5f;
+    #region Archive movement code
+    //[Header("Movement Speed")]
+    //public float speed = 5f;
+    //public float sprintSpeed = 10f;
+    //public float crouchSpeed = 2.5f;
 
-    private Quaternion startRotation;
-    private Quaternion targetLeanRotation;
+    //private Quaternion startRotation;
+    //private Quaternion targetLeanRotation;
 
-    [Header("Jumping")]
-    public float jumpForce = 10;
-    bool isGrounded = false;
+    //[Header("Jumping")]
+    //public float jumpForce = 10;
+    //bool isGrounded = false;
 
-    [Header("Leaning")]
-    public float rotationSpeed = 2;
-    public float amt;
-    public float slerpAMT;
-    public float leaningAmount = 20f;
-    public float leaningSpeed = 15f;
+    //[Header("Leaning")]
+    //public float rotationSpeed = 2;
+    //public float amt;
+    //public float slerpAMT;
+    //public float leaningAmount = 20f;
+    //public float leaningSpeed = 15f;
 
-    public Rigidbody rb;
+    //public Rigidbody rb;
 
-    private Camera cam;
-    private float camX;
+    //private Camera cam;
+    //private float camX;
+
+    //private float _verticalLookRotation = 0f;
+    //public Transform playerTransform;
+
+    //// Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //    Cursor.visible = false;
+
+    //    rb = GetComponent<Rigidbody>();
+    //    cam = GetComponentInChildren<Camera>();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    playerMovement();
+    //    camMovement();
+    //    leaning();
+    //}
+
+    //private void camMovement()
+    //{
+    //    transform.Rotate(transform.up * Input.GetAxis("Mouse X") * rotationSpeed);
+
+    //    camX -= Input.GetAxis("Mouse Y") * rotationSpeed;
+
+    //    camX = Mathf.Clamp(camX, -75, 75);
+
+    //    cam.transform.localEulerAngles = new Vector3(camX, 0, 0);
+    //}
+
+    //private void playerMovement()
+    //{
+    //    Vector3 MovementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+    //    if (Input.GetKey(KeyCode.LeftShift))
+    //    {
+    //        MovementInput *= sprintSpeed;
+    //    }
+    //    else
+    //    {
+    //        MovementInput *= speed;
+    //    }
+
+    //    Vector3 MoveDir = (transform.forward * MovementInput.z + transform.right * MovementInput.x) / 2;
+    //    rb.linearVelocity = new Vector3(MoveDir.x, rb.linearVelocity.y, MoveDir.z);
+
+    //    isGrounded = Physics.Raycast(transform.position, -transform.up, 1.3f);
+
+    //    if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+    //    {
+    //        rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+    //    }
+    //}
+
+    //private void leaning()
+    //{
+    //    float leanInput = Input.GetAxis("Lean Axes");
+
+    //    if (leanInput < 0) // Lean right
+    //    {
+    //        targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, -leaningAmount);
+    //    }
+    //    else if (leanInput > 0) // Lean left
+    //    {
+    //        targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, leaningAmount);
+    //    }
+    //    else // No lean
+    //    {
+    //        targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, 0 * leaningSpeed * Time.deltaTime);
+    //    }
+
+    //    transform.localRotation = Quaternion.Slerp(transform.localRotation, targetLeanRotation, Time.deltaTime * leaningSpeed);
+    //}
+    #endregion
+
+    public float moveSpeed;
+    public float sprintSpeed;
+    public float mouseSensitivity = 3f;
 
     private float _verticalLookRotation = 0f;
-    public Transform playerTransform;
+    public Transform cameraTransform;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        rb = GetComponent<Rigidbody>();
-        cam = GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerMovement();
-        camMovement();
-        leaning();
-    }
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-    private void camMovement()
-    {
-        transform.Rotate(transform.up * Input.GetAxis("Mouse X") * rotationSpeed);
+        transform.Rotate(Vector3.up * mouseX); // Rotate the player horizontally
 
-        camX -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        _verticalLookRotation -= mouseY;
+        _verticalLookRotation = Mathf.Clamp(_verticalLookRotation, -85, 85f);
+        cameraTransform.localRotation = Quaternion.Euler(_verticalLookRotation, 0f, 0f);
 
-        camX = Mathf.Clamp(camX, -75, 75);
+        Vector3 moveDir = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
 
-        cam.transform.localEulerAngles = new Vector3(camX, 0, 0);
-    }
-
-    private void playerMovement()
-    {
-        Vector3 MovementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            MovementInput *= sprintSpeed;
-        }
-        else
-        {
-            MovementInput *= speed;
-        }
-
-        Vector3 MoveDir = (transform.forward * MovementInput.z + transform.right * MovementInput.x) / 2;
-        rb.linearVelocity = new Vector3(MoveDir.x, rb.linearVelocity.y, MoveDir.z);
-
-        isGrounded = Physics.Raycast(transform.position, -transform.up, 1.3f);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
-        }
-    }
-
-    private void leaning()
-    {
-        float leanInput = Input.GetAxis("Lean Axes");
-
-        if (leanInput < 0) // Lean right
-        {
-            targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, -leaningAmount);
-        }
-        else if (leanInput > 0) // Lean left
-        {
-            targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, leaningAmount);
-        }
-        else // No lean
-        {
-            targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, 0 * leaningSpeed * Time.deltaTime);
-        }
-
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetLeanRotation, Time.deltaTime * leaningSpeed);
+        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
+        transform.position += moveDir * speed * Time.deltaTime;
     }
 }
