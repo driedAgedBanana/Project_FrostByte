f5f99d6d6d83e093f0095c59e7ab91a4eb60e59a
driedAgedBanana <b.cao@sintlucasedu.nl>
2025-02-09 23:09:30 +0100

    A fully rework
    Go back to the classic instantiate and remove scriptable object

--- Project_FrostByte/Assets/Scripts/Weapon/WeaponData.cs
+++ Project_FrostByte/Assets/Scripts/Weapon/WeaponData.cs
@@ -1,33 +1,32 @@
-using UnityEngine;
+//using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapons/New Weapon")]
+//[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapons/New Weapon")]
 
-public class WeaponData : ScriptableObject
-{
-    public string weaponName;
+//public class WeaponData : ScriptableObject
+//{
+//    public string weaponName;
 
-    public GameObject weapon;
-    public GameObject bulletPrefab;
+//    public GameObject bulletPrefab;
 
-    [Header("Weapon Stats")]
-    public int maxAmmo;
-    public float fireRate;
-    public float reloadTime;
+//    [Header("Weapon Stats")]
+//    public int maxAmmo;
+//    public float fireRate;
+//    public float reloadTime;
 
-    [Header("Aiming Mechanics")]
-    public float aimingSpeed;
-    public float spreadAngle;
-    public float hipfireSpreadAngle;
+//    [Header("Aiming Mechanics")]
+//    public float aimingSpeed;
+//    public float spreadAngle;
+//    public float hipfireSpreadAngle;
 
-    [Header("Recoil & Firing Behavior")]
-    public float recoilAmount;
-    public bool isAutomatic;
-    public bool isPistolWeapon;
-    public bool isBurst; 
-    public int burstCount = 3; 
-    public float burstFireRate = 0.1f; 
-    public float burstCooldown = 0.5f; 
+//    [Header("Recoil & Firing Behavior")]
+//    public float recoilAmount;
+//    public bool isAutomatic;
+//    public bool isPistolWeapon;
+//    public bool isBurst;
+//    public int burstCount = 3;
+//    public float burstFireRate = 0.1f;
+//    public float burstCooldown = 0.5f;
 
-    [Header("Audio & Visuals")]
-    public AudioClip shootingSFX;
-}
+//    [Header("Audio & Visuals")]
+//    public AudioClip shootingSFX;
+//}
--- Project_FrostByte/Assets/Scripts/Weapon/WeaponManager.cs
+++ Project_FrostByte/Assets/Scripts/Weapon/WeaponManager.cs
@@ -1,294 +1,101 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngineInternal;
 
-public class WeaponManager : MonoBehaviour
+public abstract class WeaponManager : MonoBehaviour
 {
-    [SerializeField] private Transform _mainCamera;
-    [SerializeField] private Camera _cam;
+    [Header("Weapon Settings")]
+    public string weaponName;
+    public GameObject bulletPrefab; 
 
-    [Header("Related Shooting Mechanic")]
+    [Header("Stats")]
+    public int maxAmmo = 10;
+    public float fireRate = 0.2f;
+    public float reloadTime = 2f;
 
-    // Aiming Mechanics
-    private KeyCode _aimKey = KeyCode.Mouse1;
-
-    public Transform gun;
+    [Header("Aiming, Shooting & Recoil")]
+    public float aimingSpeed = 10f;
     public Transform aimingPosition;
-    public Transform originalWeaponPosition;
+    public Transform originalPosition;
+    public AudioClip shootingSFX;
+    public AudioClip reloadingSFX;
+    public AudioSource audioSource;
 
-    private bool _isAiming = false;
+    [Header("References")]
+    [SerializeField] protected Transform shootingPoint;
+    protected KeyCode shootKey = KeyCode.Mouse0;
+    protected KeyCode aimKey = KeyCode.Mouse1;
+    protected KeyCode reloadKey = KeyCode.R;
 
-    private Animator _animator;
-    private AudioSource _shootingSFX;
+    protected int currentAmmo;
+    protected bool isReloading;
+    protected float nextFireTime;
 
-    // Shooting Mechanics
-    [SerializeField] private bool _canFire;
-
-    private KeyCode _shootKey = KeyCode.Mouse0;
-    private KeyCode _switchingFireMode = KeyCode.B;
-    
-    public WeaponData currentWeapon;
-    private int _currentAmmo;
-    private float _nextFireTime;
-
-    [SerializeField] private Transform _bulletSpawnPoint;
-
-    // Burst Mode Mechanics
-    private int _shotsInBursts;
-    private bool _canFireBurst;
-    private float _lastBurstTime;
-
-    // Reload Mechanics
-    private float _reloadingTime;
-    private bool _isReloading;
-
-
-    public Transform _weapon;
-    [SerializeField] private float _recoilSpeedReset = 5f;
-    private Vector3 _originalRotation;
-    private float _currentRecoil = 0f;
-
-    [Header("Weapon Sway mechanic")]
-    public float Drag = 2.5f;
-    public float DragThreshold = 6f;
-    public float Smoothness = 5f;
-    public Transform Parent;
-
-    private Quaternion _localRotation;
-
-    private Vector3 _originalPosition;
-
-    private void Start()
+    protected virtual void Start()
     {
-        _currentAmmo = currentWeapon.maxAmmo;
-        _mainCamera = GameObject.FindWithTag("MainCamera").transform;
+        currentAmmo = maxAmmo;
+        transform.position = originalPosition.position;
+        transform.rotation = originalPosition.rotation;
 
-        _localRotation = transform.localRotation;
-        _originalRotation = _weapon.localEulerAngles;
-
-        gun.position = originalWeaponPosition.position;
-        gun.rotation = originalWeaponPosition.rotation;
-
-        _animator = GetComponentInChildren<Animator>();
-        _shootingSFX = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    private void Update()
+    protected virtual void Update()
     {
-        Aiming();
-        WeaponSway();
-
-        HandleFireModes();
-
-        if (_canFire && !_isReloading && _currentAmmo > 0 && Input.GetKey(_shootKey))
-        {
-            Shoot();
-            _nextFireTime = Time.time + currentWeapon.fireRate;
-            ApplyRecoil();
-        }
-
-        _animator.SetBool("IsShooting", _canFire);
-        ResetRecoil();
+        HandleFire();
+        HandleAiming();
     }
 
+    public abstract void HandleFire(); // Implemented in child classes
 
-    private void HandleFireModes()
+    protected virtual void Shoot()
     {
-        if (!_isReloading && _currentAmmo > 0 && Time.time >= _nextFireTime)
+        if (currentAmmo <= 0 || isReloading) return;
+
+        GameObject bullet = Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
+        BulletScript bulletScript = bullet.GetComponent<BulletScript>();
+
+        if (bulletScript != null)
         {
-            _canFire = true;
+            bulletScript.SetDirection(shootingPoint.forward);
         }
-        else
-        {
-            _canFire = false;
-        }
-
-        // Handle automatic fire mode
-        if (currentWeapon.isAutomatic)
-        {
-            _canFire = CanFireAutomatically();
-        }
-        // Handle burst fire mode
-        else if (currentWeapon.isBurst && !currentWeapon.isPistolWeapon)
-        {
-            _canFire = CanFireBurst();
-        }
-        // Handle pistol mode
-        else if (currentWeapon.isPistolWeapon)
-        {
-            // For pistols, fire on press and only once per press (no automatic fire)
-            if (Input.GetKeyDown(_shootKey) && Time.time >= _nextFireTime)
-            {
-                Shoot();
-                _nextFireTime = Time.time + currentWeapon.fireRate; 
-                _canFire = true;
-            }
-        }
-    }
-
-
-    private bool CanFireAutomatically()
-    {
-        return Input.GetKey(_shootKey) && Time.time >= _nextFireTime; 
-    }
-
-    private bool CanFireBurst()
-    {
-        if (Input.GetKeyDown(_shootKey) && Time.time >= _lastBurstTime + currentWeapon.burstCooldown)
-        {
-            StartCoroutine(BurstFire());
-            return true;
-        }
-        return false;
-    }
-
-
-    private void Shoot()
-    {
-        if (_currentAmmo <= 0) return;
-
-        GameObject bullet = BulletPool.instance.GetBullet();
-
-        // Set bullet's position and rotation
-        bullet.transform.position = _bulletSpawnPoint.position;
-        bullet.transform.rotation = _bulletSpawnPoint.rotation;
-        bullet.SetActive(true); // Make sure the bullet is active
 
         PlayShootingSFX();
+        currentAmmo--;
 
-        _currentAmmo--;
-
-        if (_currentAmmo == 0 || Input.GetKeyDown(KeyCode.R))
+        if (currentAmmo == 0 || Input.GetKey(reloadKey))
         {
-            StartCoroutine(Reloading());
+            StartCoroutine(Reload());
         }
+    }
 
-        Vector3 targetPoint;
-        Ray ray = _cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); // Center of the screen
-        if (Physics.Raycast(ray, out RaycastHit hit))
+    protected IEnumerator Reload()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(reloadTime);
+        
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
+    protected void HandleAiming()
+    {
+        if (Input.GetKey(aimKey))
         {
-            targetPoint = hit.point;
+            transform.position = Vector3.Lerp(transform.position, aimingPosition.position, Time.deltaTime * aimingSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, aimingPosition.rotation, Time.deltaTime * aimingSpeed);
         }
         else
         {
-            // If the raycase does not hit anything, use a point far away in the camera's orward direction
-            targetPoint = ray.GetPoint(1000);
-        }
-
-        // Calculate the direction
-        Vector3 direction = (targetPoint - _bulletSpawnPoint.position).normalized;
-
-        // Apply a random range as recoil if not ADS
-        float spreadAngle = _isAiming ? currentWeapon.spreadAngle : currentWeapon.hipfireSpreadAngle;
-
-        if (spreadAngle > 0)
-        {
-            // Giving random rotation within the spread angle
-            direction = Quaternion.Euler(Random.Range(-spreadAngle, spreadAngle), Random.Range(-spreadAngle, spreadAngle), 0) * direction;
-        }
-
-        // Set the bullet's rotation to face the target rotation
-        bullet.transform.rotation = Quaternion.LookRotation(-direction); // This is some diabolical work but I do not care
-
-        // Clear the TrailRenderer right after the bullet is activated
-        TrailRenderer trail = bullet.GetComponent<TrailRenderer>();
-        if (trail != null)
-        {
-            trail.Clear(); // This will clear the trail when the bullet is fired
+            transform.position = Vector3.Lerp(transform.position, originalPosition.position, Time.deltaTime * aimingSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, originalPosition.rotation, Time.deltaTime * aimingSpeed);
         }
     }
 
-    private IEnumerator BurstFire()
+    protected void PlayShootingSFX()
     {
-        for (_shotsInBursts = 0; _shotsInBursts < currentWeapon.burstCount; _shotsInBursts++)
+        if (shootingSFX != null)
         {
-            Shoot();
-            yield return new WaitForSeconds(currentWeapon.burstFireRate);
-        }
-        _lastBurstTime = Time.time;
-    }
-
-    private void PlayShootingSFX()
-    {
-        if (currentWeapon.shootingSFX != null)
-        {
-            // One shot won't overlap with previous sounds when shooting auto
-            _shootingSFX.PlayOneShot(currentWeapon.shootingSFX);
-        }
-    }
-
-    private void ApplyRecoil()
-    {
-        _currentRecoil += currentWeapon.recoilAmount;
-        _weapon.localEulerAngles = new Vector3(_originalRotation.x - _currentRecoil, _originalRotation.y, _originalRotation.x);
-    }
-
-    private void ResetRecoil()
-    {
-        if (_currentRecoil > 0)
-        {
-            _currentRecoil = Mathf.Lerp(_currentRecoil, 0, Time.deltaTime * _recoilSpeedReset);
-            _weapon.localEulerAngles = new Vector3(_originalRotation.x - _currentRecoil, _originalRotation.y, _originalRotation.x);
-        }
-    }
-
-    IEnumerator Reloading()
-    {
-        _canFire = false;
-        _isReloading = true;
-
-        yield return new WaitForSeconds(currentWeapon.reloadTime);
-
-        _currentAmmo = currentWeapon.maxAmmo;
-        _isReloading = false;
-        _canFire = true;
-    }
-
-    private void WeaponSway()
-    {
-        float z = (Input.GetAxis("Mouse Y")) * Drag;
-        float y = (Input.GetAxis("Mouse X")) * Drag;
-
-        if (Drag >= 0)
-        {
-            y = Mathf.Clamp(y, -DragThreshold, DragThreshold);
-            z = Mathf.Clamp(z, -DragThreshold, DragThreshold);
-        }
-        else
-        {
-            y = Mathf.Clamp(y, DragThreshold, -DragThreshold);
-            z = Mathf.Clamp(z, DragThreshold, -DragThreshold);
-        }
-
-        Quaternion newRotation = Quaternion.Euler(_localRotation.x, _localRotation.y + y, Parent.localEulerAngles.z + _localRotation.z + z);
-        transform.localRotation = Quaternion.Lerp(transform.localRotation, newRotation, Time.deltaTime * Smoothness);
-    }
-
-    private void Aiming()
-    {
-        if (Input.GetKeyDown(_aimKey))
-        {
-            _isAiming = true;
-            Debug.Log("Aim!");
-        }
-        else if (Input.GetKeyUp(_aimKey))
-        {
-            _isAiming = false;
-            Debug.Log("Stop aiming");
-        }
-
-        if (_isAiming)
-        {
-            // Lerp the position and rotation based on aiming
-            gun.position = Vector3.Lerp(gun.position, aimingPosition.position, Time.deltaTime * currentWeapon.aimingSpeed);
-            gun.rotation = Quaternion.Lerp(gun.rotation, aimingPosition.rotation, Time.deltaTime * currentWeapon.aimingSpeed);
-        }
-        else
-        {
-            // Lerp back to the original position and rotation
-            gun.position = Vector3.Lerp(gun.position, originalWeaponPosition.position, Time.deltaTime * currentWeapon.aimingSpeed);
-            gun.rotation = Quaternion.Lerp(gun.rotation, originalWeaponPosition.rotation, Time.deltaTime * currentWeapon.aimingSpeed);
+            audioSource.PlayOneShot(shootingSFX);
         }
     }
 }
