1f41f943c5dd6b2f35745c693bdd1c2f508cdc34
driedAgedBanana <b.cao@sintlucasedu.nl>
2025-02-10 17:15:31 +0100

    Weapon sway rework
    First rework step

---
+++ Project_FrostByte/Assets/Scripts/Weapon/WeaponBaseManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public abstract class WeaponBaseManager : MonoBehaviour
+{
+    //[Header("Weapon Setting")]
+    //public string weaponName;
+    //[SerializeField] private Transform _mainCamera;
+    //[SerializeField] private Camera _cam;
+
+    //[Header("Reloading")]
+    //public int maxAmmo;
+    //[SerializeField] private int _currentAmmo;
+    //public float fireRate;
+    //public float reloadTime;
+    //protected bool p_canFire;
+
+    //[Header("Aiming, Shooting and Recoil")]
+    //private KeyCode _shootKey = KeyCode.Mouse0;
+    //private KeyCode _aimKey = KeyCode.Mouse1;
+
+    //// Aiming and shooting
+    //public GameObject bulletPrefab;
+    //public Transform shootingPoint;
+    //protected bool p_isAiming;
+    //public float aimingSpeed;
+    //public Transform aimingPosition;
+    //public Transform defaultPosition;
+
+    //// Recoil
+    //private float _currentRecoil;
+    //public float recoilAmount;
+    //public float spreadAngle;
+    //public float hipfireSpreadingAngle;
+    //[SerializeField] protected float p_recoilSpeedReset;
+
+    //// Audio
+    //public AudioClip shootingSFX;
+    //public AudioClip reloadingSFX;
+    //public AudioSource audioSource;
+
+    [Header("Weapon Sway")]
+    public float swayAmount;
+    public float maxSwayAmount;
+    public float swaySmoothness;
+
+    // Rotation sway
+    public float rotationSwayAmount;
+    public float maxRotationSway;
+    public float rotationSmoothness;
+
+    private Vector3 _initialPosition;
+    private Quaternion _initialRotation;
+
+    protected virtual void Start()
+    {
+        //_currentAmmo = maxAmmo;
+        //_mainCamera = GameObject.FindWithTag("MainCamera").transform;
+
+
+
+        _initialPosition = transform.localPosition;
+        _initialRotation = transform.localRotation;
+    }
+
+    protected virtual void Update()
+    {
+        HandleWeaponSway();
+    }
+
+    protected void HandleWeaponSway()
+    {
+        float l_mouseX = Input.GetAxis("Mouse X");
+        float l_mouseY = Input.GetAxis("Mouse Y");
+
+        // Position Sway
+        Vector3 l_targetPosition = new Vector3(Mathf.Clamp(l_mouseX * swayAmount, -maxSwayAmount, swayAmount), Mathf.Clamp(l_mouseY * swayAmount, -maxSwayAmount, maxSwayAmount), 0f);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, _initialPosition + l_targetPosition, Time.deltaTime * swaySmoothness);
+
+        //Rotation Sway
+        Quaternion l_targetRotation = Quaternion.Euler(Mathf.Clamp(-l_mouseY * rotationSwayAmount, -maxRotationSway, maxRotationSway),
+            Mathf.Clamp(l_mouseX * rotationSwayAmount, -maxRotationSway, maxRotationSway),
+            Mathf.Clamp(l_mouseX * rotationSwayAmount, -maxRotationSway, maxRotationSway));
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, _initialRotation * l_targetRotation, Time.deltaTime * rotationSmoothness);
+    }
+}
