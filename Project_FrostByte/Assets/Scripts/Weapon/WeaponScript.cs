3bf00752d50154b050db29b5efb65127323ae39e
dryAgedBanana <127958189+driedAgedBanana@users.noreply.github.com>
2025-02-11 15:37:55 +0100

    Fixing aiming

--- Project_FrostByte/Assets/Scripts/Weapon/WeaponScript.cs
+++ Project_FrostByte/Assets/Scripts/Weapon/WeaponScript.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
 {
+    [Header("Aiming Mechanics")]
+    public Transform weapon;
+    public Transform defaultPos;
+    public Transform aimingPos;
+    public float aimSpeed;
+
+    float AimTime;
+
+
+    [Header("Shooting Mechanics")]
     public int damage;
     public float fireRate;
     public float range;
@@ -16,16 +26,40 @@ public class WeaponScript : MonoBehaviour
     private void Start()
     {
         _cam = GetComponentInParent<Camera>();
+
+        weapon.position = defaultPos.position;
+        weapon.rotation = defaultPos.rotation;
     }
 
     private void Update()
     {
+        Aiming();
+        Shoot();
+    }
+
+    private void Aiming()
+    {
+        if (Input.GetKey(KeyCode.Mouse1) && AimTime < 1)
+        {
+            AimTime += Time.deltaTime * aimSpeed;
+        }
+        else if(!Input.GetKey(KeyCode.Mouse1) && AimTime > 0)
+        {
+            AimTime -= Time.deltaTime * aimSpeed;
+        }
+
+        weapon.position = Vector3.Lerp(defaultPos.position, aimingPos.position, AimTime);
+        weapon.rotation = Quaternion.Lerp(defaultPos.rotation, aimingPos.rotation, AimTime);
+    }
+
+    private void Shoot()
+    {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Instantiate(shootingLine, shootingPoint.position, shootingPoint.rotation);
-            if(Physics.Raycast(_cam.transform.position, _cam.transform.forward, out RaycastHit Hit))
+            if (Physics.Raycast(_cam.transform.position, _cam.transform.forward, out RaycastHit Hit))
             {
-                
+
             }
         }
     }
